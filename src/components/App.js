013ebd5313e2c1c179eed20bc04c5f458f672157
kumek <kumek@kumek.pl>
2016-08-13 01:43:04 +0200

    Application structure is done. Using webpack, stuff is building

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Someone will be hanging soon ... Now just app is being built</div>
+        )
+    };
+}
+
+export default App;
