4d08863b9ea284d3caeb323f0b7975d784502c57
Adrian Kuma <kuma.adrian@gmail.com>
2016-08-25 22:31:21 +0200

    Added shouldComponentUpdate function to WordLetter component

--- src/components/Word/WordLetter.js
+++ src/components/Word/WordLetter.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const WordLetter = (props) => {
-    return (
-        <div className={typeof props.type == 'undefined' ? 'letter empty' : 'letter filled'}>
-            {props.type ? props.letter : ''}
-        </div>
-    );
-};
+class WordLetter extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        return nextProps.type !== this.props.type;
+    }
+
+    render() {
+        return (
+            <div className={typeof this.props.type === 'undefined' ? 'letter empty' : 'letter filled'}>
+                {this.props.type ? this.props.letter : ''}
+            </div>
+        )};
+}
 
 export default WordLetter;
